656014ca0378c5dd98c5a54995959526c055315c
imhr07 <144088005+imhr07@users.noreply.github.com>
2026-02-14 00:35:21 +0530

    Update config.php

--- Velocity Rentals/config.php
+++ Velocity Rentals/config.php
@@ -5,9 +5,10 @@ $password   = getenv("MYSQLPASSWORD");
 $dbname     = getenv("MYSQLDATABASE");
 $port       = getenv("MYSQLPORT");
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
-
-if ($conn->connect_error) {
-    die("Database Connection Failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Database Connection Failed: " . $e->getMessage());
 }
 ?>
